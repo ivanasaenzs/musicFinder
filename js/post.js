7921efa350f7be06c808ef36764a1deea07442df
ivanasaenzs <84483098+ivanasaenzs@users.noreply.github.com>
2024-04-25 22:26:19 -0300

    added the post method to add new cards

--- js/post.js
+++ js/post.js
@@ -27,3 +27,42 @@ const showAddForm = () => {
 
 $("#cancel-add").addEventListener("click", cancelAdd);
 $("#add-btn-nav").addEventListener("click", showAddForm);
+
+// POST METHOD
+$("#add-artist-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const newArtist = {
+    name: $("#artist-name").value,
+    biography: $("#add-biography").value,
+    genre: $("#add-genre").value,
+    origin: $("#add-artist-origin").value,
+    members: $("#add-type-members").value,
+    image: $("#artist-img").value,
+  };
+
+  fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(newArtist),
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then((data) => {
+      if (data) {
+        $("#add-artist-form").reset();
+        $("#add-artist-form").classList.add("hidden");
+        $("#all-cards").classList.remove("hidden");
+        getArtist(baseUrl);
+      } else {
+        alert("Oops! An error has occurred");
+      }
+    })
+    .catch((err) => alert(err));
+  console.log(newArtist);
+});
