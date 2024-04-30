36e52a65fe2376e9c313c4c543ea27677ae4d70f
ivanasaenzs <84483098+ivanasaenzs@users.noreply.github.com>
2024-04-30 13:56:32 -0300

    finished delete method - handle rendering and hiding of modal and added event listeners to its buttons

--- js/delete.js
+++ js/delete.js
@@ -3,11 +3,41 @@ const deleteArtist = (artistId) => {
   fetch(`${baseUrl}/${artistId}`, {
     method: "DELETE",
   })
-    .then((res) => res.json())
-    .then((data) => getArtists(data))
-    .catch((err) => console.error(err));
+    .then((res) => {
+      if (res.ok) {
+        console.log("Artist deleted!");
+        backToHomepage();
+      }
+    })
+    .catch((err) => {
+      console.error("Error deleting artist:", err);
+    });
 };
 
-$("#all-cards").innerHTML += `
+// Function to assign event to the 'Delete' button
+const assignEventToDeleteButton = (artistId) => {
+  $(".delete-btn").addEventListener("click", (e) => {
+    console.log("Delete button clicked!", e.currentTarget.dataset.cardid);
+    showDeleteModal(artistId);
+  });
+};
 
-`;
+const showDeleteModal = (artistId) => {
+  if ($(".delete-modal-container")) {
+    $(".delete-modal-container").classList.remove("hidden");
+    // Update the data-cardid attribute of the confirm-delete button
+    $(".confirm-delete").dataset.cardid = artistId;
+  }
+};
+
+// Event listener for cancel button inside the modal
+$(".confirm-cancel").addEventListener("click", () => {
+  $(".delete-modal-container").classList.add("hidden");
+});
+
+// Event listener for confirm delete button inside the modal
+$(".confirm-delete").addEventListener("click", (e) => {
+  $(".delete-modal-container").classList.add("hidden");
+  console.log("Deleting artist with ID:", e.currentTarget.dataset.cardid);
+  deleteArtist(e.currentTarget.dataset.cardid);
+});
