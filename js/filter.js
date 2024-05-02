979ebe0ab50f8aa56c918c637dce6d97a7d2f171
ivanasaenzs <84483098+ivanasaenzs@users.noreply.github.com>
2024-05-02 16:48:59 -0300

    added event listeners to handle the filtering of categories (genre, origin, type of members, active since) and to clear the search

--- js/filter.js
+++ js/filter.js
@@ -1,28 +1,68 @@
 // Search by filter
-const searchFilters = document.getElementById("search-filters");
-const genreFilter = document.getElementById("search-genre");
-const activeSinceFilter = document.getElementById("search-active-since");
-const originFilter = document.getElementById("origin-search");
-const membersFilter = document.getElementById("search-members");
 let mainFilter = "";
+let userPickedFilter = "";
 
-searchFilters.addEventListener("change", () => {
+// Event listener to handle filter selection change (render/hide)
+$("#search-filters").addEventListener("change", () => {
   console.log("Search filters clicked");
-  mainFilter = searchFilters.value;
+  mainFilter = $("#search-filters").value;
   console.log(mainFilter);
 
-  genreFilter.classList.add("hidden");
-  activeSinceFilter.classList.add("hidden");
-  originFilter.classList.add("hidden");
-  membersFilter.classList.add("hidden");
+  $("#search-genre").classList.add("hidden");
+  $("#search-active-since").classList.add("hidden");
+  $("#origin-search").classList.add("hidden");
+  $("#search-members").classList.add("hidden");
 
   if (mainFilter === "genre") {
-    genreFilter.classList.remove("hidden");
+    $("#search-genre").classList.remove("hidden");
   } else if (mainFilter === "formation-period") {
-    activeSinceFilter.classList.remove("hidden");
+    $("#search-active-since").classList.remove("hidden");
   } else if (mainFilter === "country") {
-    originFilter.classList.remove("hidden");
+    $("#origin-search").classList.remove("hidden");
   } else if (mainFilter === "members") {
-    membersFilter.classList.remove("hidden");
+    $("#search-members").classList.remove("hidden");
   }
+});
+
+// Event listener to handle search button click and filter
+$(".search-btn").addEventListener("click", () => {
+  const filterUrl = new URLSearchParams(baseUrl.search);
+
+  // Depending on the selected mainFilter, create the fetchUrl with userPickedFilter
+  const mainFilter = $("#search-filters").value;
+  if (mainFilter === "genre") {
+    const genre = $("#search-genre").value;
+    if (genre !== "searchby-genre") {
+      filterUrl.set("genre", genre);
+    }
+  } else if (mainFilter === "formation-period") {
+    const activeSince = $("#search-active-since").value;
+    filterUrl.set("activeSince", activeSince);
+  } else if (mainFilter === "country") {
+    const origin = $("#origin-search").value;
+    filterUrl.set("origin", origin);
+  } else if (mainFilter === "members") {
+    const members = $("#search-members").value;
+    filterUrl.set("members", members);
+  }
+
+  $("#all-cards").innerHTML = "";
+  getArtists(`${baseUrl}/?${filterUrl}`);
+});
+
+$(".clear-btn").addEventListener("click", () => {
+  // Reset select elements to default and add hidden class
+  $("#search-filters").value = "search-by";
+  $("#search-genre").value = "searchby-genre";
+  $("#search-active-since").value = "active-since";
+  $("#origin-search").value = "searchby-origin";
+  $("#search-members").value = "searchby-members";
+
+  $("#search-genre").classList.add("hidden");
+  $("#search-active-since").classList.add("hidden");
+  $("#origin-search").classList.add("hidden");
+  $("#search-members").classList.add("hidden");
+
+  $("#all-cards").innerHTML = "";
+  getArtists(baseUrl);
 });
